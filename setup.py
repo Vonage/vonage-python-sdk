4153ae55453d0c6faa1d0bbf662618d2dbe166ac
Tim Craft <mail@timcraft.com>
2015-04-30 17:09:00 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='nexmo',
+  version='1.0.0',
+  description='A Python wrapper for the Nexmo API',
+  long_description='A Python wrapper for the Nexmo API',
+  url='http://github.com/timcraft/python-nexmo',
+  author='Tim Craft',
+  author_email='mail@timcraft.com',
+  license='LGPL-3.0',
+  packages=['nexmo'],
+  platforms=['any'],
+  install_requires=['requests'])

02ac38532cea722ef71717cbe70f9639a4373d07
Tim Craft <mail@timcraft.com>
2016-04-24 16:42:51 +0100

    Tweak description and long_description

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ with open('nexmo/__init__.py', 'r') as fd:
 
 setup(name='nexmo',
   version=version,
-  description='Python client for the Nexmo API',
-  long_description='Python client for the Nexmo API',
+  description='Nexmo Client Library for Python',
+  long_description='This is the Python client library for Nexmo\'s API. To use it you\'ll need a Nexmo account. Sign up `for free at nexmo.com <http://nexmo.com?src=python-client-library>`_.',
   url='http://github.com/Nexmo/nexmo-python',
   author='Tim Craft',
   author_email='mail@timcraft.com',

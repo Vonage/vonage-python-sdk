cf1358a48ad39530154296e07fe509e11367c631
Tim Craft <mail@timcraft.com>
2016-05-10 17:28:10 +0100

    Add api_host attribute and refactor

--- nexmo/__init__.py
+++ nexmo/__init__.py
@@ -1,11 +1,6 @@
 __version__ = '1.1.0'
 
 
-try:
-  from urllib.parse import urljoin
-except ImportError:
-  from urlparse import urljoin
-
 import requests, os
 
 from platform import python_version
@@ -29,96 +24,98 @@ class Client():
 
     self.host = 'rest.nexmo.com'
 
+    self.api_host = 'api.nexmo.com'
+
   def send_message(self, params):
-    return self.post('/sms/json', params)
+    return self.post(self.host, '/sms/json', params)
 
   def get_balance(self):
-    return self.get('/account/get-balance')
+    return self.get(self.host, '/account/get-balance')
 
   def get_country_pricing(self, country_code):
-    return self.get('/account/get-pricing/outbound', {'country': country_code})
+    return self.get(self.host, '/account/get-pricing/outbound', {'country': country_code})
 
   def get_prefix_pricing(self, prefix):
-    return self.get('/account/get-prefix-pricing/outbound', {'prefix': prefix})
+    return self.get(self.host, '/account/get-prefix-pricing/outbound', {'prefix': prefix})
 
   def get_account_numbers(self, params=None, **kwargs):
-    return self.get('/account/numbers', params or kwargs)
+    return self.get(self.host, '/account/numbers', params or kwargs)
 
   def get_available_numbers(self, country_code, params=None, **kwargs):
-    return self.get('/number/search', dict(params or kwargs, country=country_code))
+    return self.get(self.host, '/number/search', dict(params or kwargs, country=country_code))
 
   def buy_number(self, params=None, **kwargs):
-    return self.post('/number/buy', params or kwargs)
+    return self.post(self.host, '/number/buy', params or kwargs)
 
   def cancel_number(self, params=None, **kwargs):
-    return self.post('/number/cancel', params or kwargs)
+    return self.post(self.host, '/number/cancel', params or kwargs)
 
   def update_number(self, params=None, **kwargs):
-    return self.post('/number/update', params or kwargs)
+    return self.post(self.host, '/number/update', params or kwargs)
 
   def get_message(self, message_id):
-    return self.get('/search/message', {'id': message_id})
+    return self.get(self.host, '/search/message', {'id': message_id})
 
   def get_message_rejections(self, params=None, **kwargs):
-    return self.get('/search/rejections', params or kwargs)
+    return self.get(self.host, '/search/rejections', params or kwargs)
 
   def search_messages(self, params=None, **kwargs):
-    return self.get('/search/messages', params or kwargs)
+    return self.get(self.host, '/search/messages', params or kwargs)
 
   def send_ussd_push_message(self, params=None, **kwargs):
-    return self.post('/ussd/json', params or kwargs)
+    return self.post(self.host, '/ussd/json', params or kwargs)
 
   def send_ussd_prompt_message(self, params=None, **kwargs):
-    return self.post('/ussd-prompt/json', params or kwargs)
+    return self.post(self.host, '/ussd-prompt/json', params or kwargs)
 
   def send_2fa_message(self, params=None, **kwargs):
-    return self.post('/sc/us/2fa/json', params or kwargs)
+    return self.post(self.host, '/sc/us/2fa/json', params or kwargs)
 
   def send_event_alert_message(self, params=None, **kwargs):
-    return self.post('/sc/us/alert/json', params or kwargs)
+    return self.post(self.host, '/sc/us/alert/json', params or kwargs)
 
   def send_marketing_message(self, params=None, **kwargs):
-    return self.post('/sc/us/marketing/json', params or kwargs)
+    return self.post(self.host, '/sc/us/marketing/json', params or kwargs)
 
   def initiate_call(self, params=None, **kwargs):
-    return self.post('/call/json', params or kwargs)
+    return self.post(self.host, '/call/json', params or kwargs)
 
   def initiate_tts_call(self, params=None, **kwargs):
-    return self.post('https://api.nexmo.com/tts/json', params or kwargs)
+    return self.post(self.api_host, '/tts/json', params or kwargs)
 
   def initiate_tts_prompt_call(self, params=None, **kwargs):
-    return self.post('https://api.nexmo.com/tts-prompt/json', params or kwargs)
+    return self.post(self.api_host, '/tts-prompt/json', params or kwargs)
 
   def send_verification_request(self, params=None, **kwargs):
-    return self.post('https://api.nexmo.com/verify/json', params or kwargs)
+    return self.post(self.api_host, '/verify/json', params or kwargs)
 
   def check_verification_request(self, params=None, **kwargs):
-    return self.post('https://api.nexmo.com/verify/check/json', params or kwargs)
+    return self.post(self.api_host, '/verify/check/json', params or kwargs)
 
   def get_verification_request(self, request_id):
-    return self.get('https://api.nexmo.com/verify/search/json', {'request_id': request_id})
+    return self.get(self.api_host, '/verify/search/json', {'request_id': request_id})
 
   def control_verification_request(self, params=None, **kwargs):
-    return self.post('https://api.nexmo.com/verify/control/json', params or kwargs)
+    return self.post(self.api_host, '/verify/control/json', params or kwargs)
 
   def get_basic_number_insight(self, params=None, **kwargs):
-    return self.get('https://api.nexmo.com/number/format/json', params or kwargs)
+    return self.get(self.api_host, '/number/format/json', params or kwargs)
 
   def get_number_insight(self, params=None, **kwargs):
-    return self.get('https://api.nexmo.com/number/lookup/json', params or kwargs)
+    return self.get(self.api_host, '/number/lookup/json', params or kwargs)
 
   def request_number_insight(self, params=None, **kwargs):
-    return self.post('/ni/json', params or kwargs)
+    return self.post(self.host, '/ni/json', params or kwargs)
 
-  def get(self, request_uri, params={}):
-    uri = urljoin('https://' + self.host, request_uri)
+  def get(self, host, request_uri, params={}):
+    uri = 'https://' + host + request_uri
 
     params = dict(params, api_key=self.api_key, api_secret=self.api_secret)
 
     return self.parse(requests.get(uri, params=params, headers=self.headers))
 
-  def post(self, request_uri, params):
-    uri = urljoin('https://' + self.host, request_uri)
+  def post(self, host, request_uri, params):
+    uri = 'https://' + host + request_uri
 
     params = dict(params, api_key=self.api_key, api_secret=self.api_secret)
 

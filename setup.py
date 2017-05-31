11d9294323bb975549608a1889e0f6616c54431d
Mark Smith <mark.smith@practicalpoetry.co.uk>
2017-05-31 16:05:02 +0100

    Add Python 3.6 as a supported version.

--- setup.py
+++ setup.py
@@ -24,4 +24,5 @@ setup(name='nexmo',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
       ])

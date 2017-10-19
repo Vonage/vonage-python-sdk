f9dfcf5612719bdbbf3f6254f68e3b80a6939d4f
Mark Smith <mark.smith@practicalpoetry.co.uk>
2017-10-19 10:19:14 +0100

    Port tests to pytest (#44)
    * All tests ported to pytest. Yay
    
    * Add instructions for running tests.
    
    * New tests structure.
    
    * Tests working again.
    
    * Adding support for coverage, so nothing gets lost.
    
    * Tests in separate files.
    
    * Code cleanup
    
    * Fix README links.
    
    * Fixes from @tommorris review.

---
+++ tests/test_numbers.py
@@ -0,0 +1,48 @@
+from util import *
+
+
+@responses.activate
+def test_get_available_numbers(client, dummy_data):
+    stub(responses.GET, 'https://rest.nexmo.com/number/search')
+
+    assert isinstance(client.get_available_numbers('CA', size=25), dict)
+    assert request_user_agent() == dummy_data.user_agent
+    assert 'country=CA' in request_query()
+    assert 'size=25' in request_query()
+
+
+@responses.activate
+def test_buy_number(client, dummy_data):
+    stub(responses.POST, 'https://rest.nexmo.com/number/buy')
+
+    params = {'country': 'US', 'msisdn': 'number'}
+
+    assert isinstance(client.buy_number(params), dict)
+    assert request_user_agent() == dummy_data.user_agent
+    assert 'country=US' in request_body()
+    assert 'msisdn=number' in request_body()
+
+
+@responses.activate
+def test_cancel_number(client, dummy_data):
+    stub(responses.POST, 'https://rest.nexmo.com/number/cancel')
+
+    params = {'country': 'US', 'msisdn': 'number'}
+
+    assert isinstance(client.cancel_number(params), dict)
+    assert request_user_agent() == dummy_data.user_agent
+    assert 'country=US' in request_body()
+    assert 'msisdn=number' in request_body()
+
+
+@responses.activate
+def test_update_number(client, dummy_data):
+    stub(responses.POST, 'https://rest.nexmo.com/number/update')
+
+    params = {'country': 'US', 'msisdn': 'number', 'moHttpUrl': 'callback'}
+
+    assert isinstance(client.update_number(params), dict)
+    assert request_user_agent() == dummy_data.user_agent
+    assert 'country=US' in request_body()
+    assert 'msisdn=number' in request_body()
+    assert 'moHttpUrl=callback' in request_body()

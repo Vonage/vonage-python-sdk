03120622d1d0d3524e234455db8ef0bf84e3e1e9
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-06-12 09:59:52 +0100

    We change versioon with bumpversion now.

--- setup.py
+++ setup.py
@@ -2,11 +2,8 @@ import re
 
 from setuptools import setup
 
-with open('nexmo/__init__.py', 'r') as fd:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE).group(1)
-
 setup(name='nexmo',
-      version=version,
+      version='2.0.0',
       description='Nexmo Client Library for Python',
       long_description='This is the Python client library for Nexmo\'s API. To use it you\'ll need a Nexmo account. Sign up `for free at nexmo.com <http://nexmo.com?src=python-client-library>`_.',
       url='http://github.com/Nexmo/nexmo-python',

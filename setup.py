041e67f0397a44bc7b455513e7a47d1ab812c206
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-09-25 11:38:43 +0100

    More formatting fixes.

--- setup.py
+++ setup.py
@@ -2,29 +2,27 @@ import re
 
 from setuptools import setup
 
-setup(name='nexmo',
-      version='2.2.0',
-      description='Nexmo Client Library for Python',
-      long_description='This is the Python client library for Nexmo\'s API. To use it you\'ll need a Nexmo account. Sign up `for free at nexmo.com <http://nexmo.com?src=python-client-library>`_.',
-      url='http://github.com/Nexmo/nexmo-python',
-      author='Nexmo',
-      author_email='devrel@nexmo.com',
-      license='MIT',
-      packages=['nexmo'],
-      platforms=['any'],
-      install_requires=[
-          'requests>=2.4.2',
-          'PyJWT[crypto]',
-          'pytz',
-      ],
-      tests_require=['cryptography'],
-      classifiers=[
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-      ])
+setup(
+    name="nexmo",
+    version="2.2.0",
+    description="Nexmo Client Library for Python",
+    long_description="This is the Python client library for Nexmo's API. To use it you'll need a Nexmo account. Sign up `for free at nexmo.com <http://nexmo.com?src=python-client-library>`_.",
+    url="http://github.com/Nexmo/nexmo-python",
+    author="Nexmo",
+    author_email="devrel@nexmo.com",
+    license="MIT",
+    packages=["nexmo"],
+    platforms=["any"],
+    install_requires=["requests>=2.4.2", "PyJWT[crypto]", "pytz"],
+    tests_require=["cryptography"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)

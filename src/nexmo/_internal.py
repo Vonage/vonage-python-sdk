4efd7a786d8d5566915473fe5a354aae6e757d08
Mark Smith <mark.smith@vonage.com>
2019-11-14 11:27:35 +0000

    Docstrings for conversations API.

--- src/nexmo/_internal.py
+++ src/nexmo/_internal.py
@@ -283,9 +283,28 @@ class Conversation(object):
         self._api_server = api_server
 
     def create_conversation(self, conversation_data):
+        """
+        Create a new conversation.
+
+        See https://developer.nexmo.com/api/conversation#createConversation for details.
+
+        :param conversation_data: A dict containing the request body configuration for the conversation to be created.
+        """
         return self._api_server.post("/v0.1/conversations", conversation_data)
 
     def list_conversations(self, page_size=None, order=None, cursor=None):
+        """
+        List all, or a filtered set, of conversations.
+
+        Only a summary of conversation data is provided.
+        Once you've identified the conversation you need to work with, use :py:func: #get_conversation: to fetch all of the conversation details.
+
+        See https://developer.nexmo.com/api/conversation#get-conversations for details.
+
+        :param int page_size: The number of items in the page to be returned
+        :param str order: Sort the conversations by date created. Either 'asc' or 'desc'.
+        :param str cursor: The identifier needed to request the page.
+        """
         if order is not None and not isinstance(order, Order):
             raise ClientError("order should be an instance of the Order enum.")
 
@@ -295,6 +314,13 @@ class Conversation(object):
         return self._api_server.get("/v0.1/conversations", params=params)
 
     def update_conversation(self, conversation):
+        """
+        Update a conversation, specified by the 'id' attribute in the supplied 'conversation' dict.
+
+        See https://developer.nexmo.com/api/conversation#update-conversation for details.
+
+        :param conversation: A dict containing the conversation details to be updated.
+        """
         conversation_id = conversation.pop("id")
         path = "/v0.1/conversations/{conversation_id}".format(
             conversation_id=conversation_id
@@ -302,43 +328,107 @@ class Conversation(object):
         return self._api_server.put(path, conversation)
 
     def get_conversation(self, conversation_id):
+        """
+        Get all details of a conversation.
+
+        See https://developer.nexmo.com/api/conversation#get-conversation for details.
+
+        :param str conversation_id: The id of the conversation to be returned.
+        """
         path = "/v0.1/conversations/{conversation_id}".format(
             conversation_id=conversation_id
         )
         return self._api_server.get(path)
 
     def delete_conversation(self, conversation_id):
+        """
+        Delete the conversation identified by 'conversation_id'.
+        """
         path = "/v0.1/conversations/{conversation_id}".format(
             conversation_id=conversation_id
         )
         return self._api_server.delete(path, headers={})
 
     def list_users(self):
+        """
+        List all, or a filtered set, of users.
+
+        See https://developer.nexmo.com/api/conversation#get-users for details.
+
+        :param int page_size: The number of items in the page to be returned
+        :param str order: Sort the users by date created. Either 'asc' or 'desc'.
+        :param str cursor: The identifier needed to request a page.
+        """
         return self._api_server.get("/v0.1/users")
 
     def create_user(self, user_data):
+        """
+        Create a new user.
+
+        See https://developer.nexmo.com/api/conversation#create-user for details.
+
+        :param user_data: A dict containing configuration for the new user.
+        """
         return self._api_server.post("/v0.1/users", user_data)
 
     def get_user(self, user_id):
+        """
+        Get a single user.
+
+        See https://developer.nexmo.com/api/conversation#get-user for details.
+
+        :param str user_id: The ID of the user to be returned.
+        """
         path = "/v0.1/users/{user_id}".format(user_id=user_id)
         return self._api_server.get(path)
 
     def update_user(self, user_data):
+        """
+        Update a user record, identified by the 'id' attribute in 'user_data'.
+
+        See https://developer.nexmo.com/api/conversation#update-user for details.
+
+        :param dict user_data: A dict containing user data attributes to be updated.
+        """
         user_id = user_data.pop("id")
         path = "/v0.1/users/{user_id}".format(user_id=user_id)
         return self._api_server.put(path, user_data)
 
     def delete_user(self, user_id):
+        """
+        Delete a user identified by 'user_id'.
+
+        See https://developer.nexmo.com/api/conversation#delete-user for details.
+
+        :param str user_id: The id of the user to be deleted.
+        """
         path = "/v0.1/users/{user_id}".format(user_id=user_id)
         return self._api_server.delete(path, headers={})
 
     def create_member(self, conversation_id, body):
+        """
+        Create a member by attaching a user to a conversation.
+
+        See https://developer.nexmo.com/api/conversation#create-member for details.
+
+        :param str conversation_id: The id of the conversation to attach the user to.
+        :param dict body: Configuration describing how the user is to be attached to the conversation.
+        """
         path = "/v0.1/conversations/{conversation_id}/members".format(
             conversation_id=conversation_id
         )
         return self._api_server.post(path, body)
 
     def list_members(self, conversation_id, page_size=None, order=None, cursor=None):
+        """
+        List the members of a conversation.
+
+        See https://developer.nexmo.com/api/conversation#get-members for details.
+
+        :param int page_size: The number of items in the page to be returned
+        :param str order: Sort the entries by date created. Either 'asc' or 'desc'.
+        :param str cursor: The identifier needed to request a page.
+        """
         path = "/v0.1/conversations/{conversation_id}/members".format(
             conversation_id=conversation_id
         )
@@ -348,30 +438,72 @@ class Conversation(object):
         return self._api_server.get(path, params=params)
 
     def get_member(self, conversation_id, member_id):
+        """
+        Get the details of a conversation's member.
+
+        See https://developer.nexmo.com/api/conversation#get-member for details.
+
+        :param str conversation_id: The id of the conversation the member belongs to.
+        :param str member_id: The id of the member.
+        """
         path = "/v0.1/conversations/{conversation_id}/members/{member_id}".format(
             conversation_id=conversation_id, member_id=member_id
         )
         return self._api_server.get(path)
 
     def update_member(self, conversation_id, member_id, body):
+        """
+        Update the details of a conversation's member.
+
+        See https://developer.nexmo.com/api/conversation#update-member for details.
+
+        :param str conversation_id: The id of the conversation the member belongs to.
+        :param str member_id: The id of the member.
+        :param dict body: A dict containing the member's configuration to be updated.
+        """
         path = "/v0.1/conversations/{conversation_id}/members/{member_id}".format(
             conversation_id=conversation_id, member_id=member_id
         )
         return self._api_server.put(path)
 
     def delete_member(self, conversation_id, member_id):
+        """
+        Remove a user from a conversation.
+
+        See https://developer.nexmo.com/api/conversation#delete-member for details.
+
+        :param str conversation_id: The id of the conversation the member belongs to.
+        :param str member_id: The id of the member.
+        """
         path = "/v0.1/conversations/{conversation_id}/members/{member_id}".format(
             conversation_id=conversation_id, member_id=member_id
         )
         return self._api_server.delete(path, headers={})
 
     def create_event(self, conversation_id, event_data):
+        """
+        Fire an event to members of the conversation.
+
+        See https://developer.nexmo.com/api/conversation#create-event for details.
+
+        :param str conversation_id: The id of the conversation the event belongs to.
+        :param dict event_data: The details of the event.
+        """
         path = "/v0.1/conversations/{conversation_id}/events".format(
             conversation_id=conversation_id
         )
         return self._api_server.post(path, event_data)
 
     def list_events(self, conversation_id, start_id=None, end_id=None):
+        """
+        List the events in a conversation.
+
+        See https://developer.nexmo.com/api/conversation#get-events for details.
+
+        :param str start_id: The first event id to be returned.
+        :param str end_id: The last event id to be returned.
+
+        """
         params = _filter_none_values({"start_id": start_id, "end_id": end_id})
         path = "/v0.1/conversations/{conversation_id}/events".format(
             conversation_id=conversation_id
@@ -379,12 +511,28 @@ class Conversation(object):
         return self._api_server.get(path, params=params)
 
     def get_event(self, conversation_id, event_id):
+        """
+        Get the details of an event.
+
+        See https://developer.nexmo.com/api/conversation#get-event for details.
+
+        :param str conversation_id: The id of the conversation the event belongs to.
+        :param str event_id: The id of the event.
+        """
         path = "/v0.1/conversations/{conversation_id}/events/{event_id}".format(
             conversation_id=conversation_id, event_id=event_id
         )
         return self._api_server.get(path)
 
     def delete_event(self, conversation_id, event_id):
+        """
+        Delete an event.
+
+        See https://developer.nexmo.com/api/conversation#delete-event for details.
+
+        :param str conversation_id: The id of the conversation the event belongs to.
+        :param str event_id: The id of the event.
+        """
         path = "/v0.1/conversations/{conversation_id}/events/{event_id}".format(
             conversation_id=conversation_id, event_id=event_id
         )
@@ -392,6 +540,9 @@ class Conversation(object):
 
 
 def _filter_none_values(d):
+    """
+    A convenient one-liner to remove items from a dict where the values are None.
+    """
     return {k: v for k, v in d.items() if v is not None}
 
 

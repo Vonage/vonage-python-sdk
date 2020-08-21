69b1d9a8e7cd84a6ade884966079bab9b4846820
superdiana <diana.rodriguezmanrique@vonage.com>
2020-08-21 11:48:53 -0400

    Release v2.5.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with io.open(
 
 setup(
     name="nexmo",
-    version="2.4.0",
+    version="2.5.0",
     description="Nexmo Client Library for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",

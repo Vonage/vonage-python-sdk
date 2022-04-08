66af36a87be8bd748eb46822af42bda8f611c159
maxkahan <maxkahan94@gmail.com>
2022-04-08 12:42:59 +0100

    require python version >=3.7 when installing from PyPI

--- setup.py
+++ setup.py
@@ -28,15 +28,14 @@ setup(
         "pytz>=2018.5",
         "Deprecated",
     ],
-    python_requires=">=3, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=3.7",
     tests_require=["cryptography>=2.3.1"],
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )

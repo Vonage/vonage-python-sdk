ecdceab55ceae265c1198444640d06c976913c4d
maxkahan <maxkahan94@gmail.com>
2022-04-19 18:48:24 +0100

    Bump version: 2.6.2 → 2.6.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with io.open(
 
 setup(
     name="vonage",
-    version="2.6.2",
+    version="2.6.3",
     description="Vonage Server SDK for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",

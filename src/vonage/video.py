91fdbe78dc2faf9489221f26c51d143effc42d44
maxkahan <maxkahan94@gmail.com>
2022-11-06 04:05:21 +0000

    added disable_mute_all_streams method

--- src/vonage/video.py
+++ src/vonage/video.py
@@ -83,6 +83,9 @@ class Video:
             auth_type=Video.auth_type
         )
 
+    def disable_mute_all_streams(self, session_id, excluded_stream_ids=[]):
+        self.mute_all_streams(session_id, active=False, excluded_stream_ids=excluded_stream_ids)
+
     def list_archives(self, filter_params=None, **filter_kwargs):
         return self._client.get(
             self._client.video_host(),

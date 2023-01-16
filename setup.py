18307818c1839fe7f65c58df0a7d4566c42a8f11
maxkahan <maxkahan94@gmail.com>
2023-01-16 01:54:07 +0000

    fixing merge conflict

--- setup.py
+++ setup.py
@@ -4,18 +4,12 @@ import os
 from setuptools import setup, find_packages
 
 
-with io.open(
-    os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8"
-) as f:
+with io.open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name="vonage",
-<<<<<<< HEAD
     version="4.0.0b0",
-=======
-    version="3.2.0",
->>>>>>> main
     description="Vonage Server SDK for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",

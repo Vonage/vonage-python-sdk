595f0d5778b758d0db061a11e9ad527eee51c2ef
maxkahan <maxkahan94@gmail.com>
2023-01-16 20:58:15 +0000

    refactoring, raising exception

--- tests/test_packages.py
+++ tests/test_packages.py
@@ -5,12 +5,8 @@ def test_subdirectories_are_python_packages():
     subdirs = [
         os.path.join('src/vonage', o) for o in os.listdir('src/vonage') if os.path.isdir(os.path.join('src/vonage', o))
     ]
-    print(subdirs)
     for subdir in subdirs:
-        if '__pycache__' in subdir:
+        if '__pycache__' in subdir or os.path.isfile(f'{subdir}/__init__.py'):
             continue
-        elif os.path.isfile(f'{subdir}/__init__.py'):
-            print(f'all good for {subdir}')
         else:
-            print(f'Subfolder {subdir} doesn\'t have an __init__.py file')
-            assert False
+            raise Exception(f'Subfolder {subdir} doesn\'t have an __init__.py file')

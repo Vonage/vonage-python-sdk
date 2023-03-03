2a490a0b3727c91a007b01f0c06774ed71f7535f
maxkahan <maxkahan94@gmail.com>
2023-03-03 02:38:51 +0000

    add sticker as a whatsapp channel, add specific errors to validation tests, increase max client_ref value

--- src/vonage/messages.py
+++ src/vonage/messages.py
@@ -8,7 +8,7 @@ class Messages:
     valid_message_types = {
         'sms': {'text'},
         'mms': {'image', 'vcard', 'audio', 'video'},
-        'whatsapp': {'text', 'image', 'audio', 'video', 'file', 'template', 'custom'},
+        'whatsapp': {'text', 'image', 'audio', 'video', 'file', 'template', 'sticker', 'custom'},
         'messenger': {'text', 'image', 'audio', 'video', 'file'},
         'viber_service': {'text', 'image', 'video', 'file'},
     }
@@ -46,7 +46,7 @@ class Messages:
         if params['channel'] not in Messages.valid_message_channels:
             raise MessagesError(
                 f"""
-            '{params['channel']}' is an invalid message channel. 
+            "{params['channel']}" is an invalid message channel. 
             Must be one of the following types: {self.valid_message_channels}'
             """
             )
@@ -75,16 +75,24 @@ class Messages:
             )
 
     def _channel_specific_checks(self, params):
-        if (params['channel'] == 'whatsapp' and params['message_type'] == 'template' and 'whatsapp' not in params) or (
-            params['channel'] == 'viber_service' and 'viber_service' not in params
+        if (
+            (params['channel'] == 'whatsapp' and params['message_type'] == 'template' and 'whatsapp' not in params)
+            or (params['channel'] == 'whatsapp' and params['message_type'] == 'sticker' and 'sticker' not in params)
+            or (params['channel'] == 'viber_service' and 'viber_service' not in params)
         ):
             raise MessagesError(
                 f'''You must specify all required properties for message channel "{params["channel"]}".'''
             )
+        elif params['channel'] == 'whatsapp' and params['message_type'] == 'sticker':
+            self._check_valid_whatsapp_sticker(params['sticker'])
 
     def _check_valid_client_ref(self, params):
         if 'client_ref' in params:
-            if len(params['client_ref']) <= 40:
+            if len(params['client_ref']) <= 100:
                 self._client_ref = params['client_ref']
             else:
-                raise MessagesError('client_ref can be a maximum of 40 characters.')
+                raise MessagesError('client_ref can be a maximum of 100 characters.')
+
+    def _check_valid_whatsapp_sticker(self, sticker):
+        if ('id' not in sticker and 'url' not in sticker) or ('id' in sticker and 'url' in sticker):
+            raise MessagesError('Must specify one, and only one, of "id" or "url" in the "sticker" field.')
--- tests/test_messages_validate_input.py
+++ tests/test_messages_validate_input.py
@@ -3,12 +3,13 @@ from vonage.errors import MessagesError
 
 
 def test_invalid_send_message_params_object(messages):
-    with pytest.raises(MessagesError):
+    with pytest.raises(MessagesError) as err:
         messages.send_message('hi')
+    assert str(err.value) == 'Parameters to the send_message method must be specified as a dictionary.'
 
 
 def test_invalid_message_channel(messages):
-    with pytest.raises(MessagesError):
+    with pytest.raises(MessagesError) as err:
         messages.send_message(
             {
                 'channel': 'carrier_pigeon',
@@ -18,24 +19,27 @@ def test_invalid_message_channel(messages):
                 'text': 'my important message',
             }
         )
+    assert '"carrier_pigeon" is an invalid message channel.' in str(err.value)
 
 
 def test_invalid_message_type(messages):
-    with pytest.raises(MessagesError):
+    with pytest.raises(MessagesError) as err:
         messages.send_message(
             {'channel': 'sms', 'message_type': 'video', 'to': '12345678', 'from': 'vonage', 'video': 'my_url.com'}
         )
+    assert '"video" is not a valid message type for channel "sms".' in str(err.value)
 
 
 def test_invalid_recipient_not_string(messages):
-    with pytest.raises(MessagesError):
+    with pytest.raises(MessagesError) as err:
         messages.send_message(
             {'channel': 'sms', 'message_type': 'text', 'to': 12345678, 'from': 'vonage', 'text': 'my important message'}
         )
+    assert str(err.value) == 'Message recipient ("to=12345678") not in a valid format.'
 
 
 def test_invalid_recipient_number(messages):
-    with pytest.raises(MessagesError):
+    with pytest.raises(MessagesError) as err:
         messages.send_message(
             {
                 'channel': 'sms',
@@ -45,17 +49,19 @@ def test_invalid_recipient_number(messages):
                 'text': 'my important message',
             }
         )
+    assert str(err.value) == 'Message recipient number ("to=+441234567890") not in a valid format.'
 
 
 def test_invalid_messenger_recipient(messages):
-    with pytest.raises(MessagesError):
+    with pytest.raises(MessagesError) as err:
         messages.send_message(
             {'channel': 'messenger', 'message_type': 'text', 'to': '', 'from': 'vonage', 'text': 'my important message'}
         )
+    assert str(err.value) == 'Message recipient ID ("to=") not in a valid format.'
 
 
 def test_invalid_sender(messages):
-    with pytest.raises(MessagesError):
+    with pytest.raises(MessagesError) as err:
         messages.send_message(
             {
                 'channel': 'sms',
@@ -65,6 +71,7 @@ def test_invalid_sender(messages):
                 'text': 'my important message',
             }
         )
+    assert str(err.value) == 'Message sender ("frm=1234") set incorrectly. Set a valid name or number for the sender.'
 
 
 def test_set_client_ref(messages):
@@ -82,7 +89,7 @@ def test_set_client_ref(messages):
 
 
 def test_invalid_client_ref(messages):
-    with pytest.raises(MessagesError):
+    with pytest.raises(MessagesError) as err:
         messages._check_valid_client_ref(
             {
                 'channel': 'sms',
@@ -90,9 +97,10 @@ def test_invalid_client_ref(messages):
                 'to': '441234567890',
                 'from': 'vonage',
                 'text': 'my important message',
-                'client_ref': 'my client reference that is far longer than the 40 character limit',
+                'client_ref': 'my client reference that is, in fact, a small, but significant amount longer than the 100 character limit imposed at this present juncture.',
             }
         )
+    assert str(err.value) == 'client_ref can be a maximum of 100 characters.'
 
 
 def test_whatsapp_template(messages):
@@ -201,7 +209,7 @@ def test_viber_service_image_action_button(messages):
 
 
 def test_incomplete_input(messages):
-    with pytest.raises(MessagesError):
+    with pytest.raises(MessagesError) as err:
         messages.validate_send_message_input(
             {
                 'channel': 'viber_service',
@@ -211,3 +219,42 @@ def test_incomplete_input(messages):
                 'text': 'my important message',
             }
         )
+    assert str(err.value) == 'You must specify all required properties for message channel "viber_service".'
+
+
+def test_whatsapp_sticker_id(messages):
+    messages.validate_send_message_input(
+        {
+            'channel': 'whatsapp',
+            'message_type': 'sticker',
+            'sticker': {'id': '13aaecab-2485-4255-a0a7-97a2be6906b9'},
+            'to': '44123456789',
+            'from': 'vonage',
+        }
+    )
+
+
+def test_whatsapp_sticker_url(messages):
+    messages.validate_send_message_input(
+        {
+            'channel': 'whatsapp',
+            'message_type': 'sticker',
+            'sticker': {'url': 'https://example.com/sticker1.webp'},
+            'to': '44123456789',
+            'from': 'vonage',
+        }
+    )
+
+
+def test_whatsapp_sticker_error(messages):
+    with pytest.raises(MessagesError) as err:
+        messages.validate_send_message_input(
+            {
+                'channel': 'whatsapp',
+                'message_type': 'sticker',
+                'sticker': {'my_sticker'},
+                'to': '44123456789',
+                'from': 'vonage',
+            }
+        )
+    assert str(err.value) == 'Must specify one, and only one, of "id" or "url" in the "sticker" field.'

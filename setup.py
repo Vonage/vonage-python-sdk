601c371b8fcf61ebccc9273f08a6e8da99932055
maxkahan <maxkahan94@gmail.com>
2023-06-02 20:30:39 +0100

    Bump version: 3.5.1 → 3.5.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with io.open(
 
 setup(
     name="vonage",
-    version="3.5.1",
+    version="3.5.2",
     description="Vonage Server SDK for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",

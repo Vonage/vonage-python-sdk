ee208f8452b7a516b30e96449b63f456b0a7b9ec
maxkahan <maxkahan94@gmail.com>
2023-06-20 02:47:30 +0100

    add checks

--- src/vonage/number_insight_v2.py
+++ src/vonage/number_insight_v2.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
+from typing import Union, Literal
 
 if TYPE_CHECKING:
     from vonage import Client
@@ -10,7 +11,16 @@ class NumberInsightV2:
         self._client = client
         self._auth_type = 'header'
 
-    def fraud_check(self, params: dict):
+    def fraud_check(self, number: str, insights: Union[str, list]):
+        params = {'type': 'phone', 'phone': number}
+
+        if type(insights) == str:
+            if insights == 'fraud_score' or insights == 'sim_swap':
+                params['insights'] = [insights]
+        elif type(insights) == 'list':
+            # need to add validation to this particular section
+            params['insights'] == insights
+        print(params)
         return self._client.post(
             self._client.api_host(), '/v2/ni', params, auth_type=self._auth_type
         )
